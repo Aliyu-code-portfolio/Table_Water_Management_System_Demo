1507891279fd78b2798ba4c892fc12cb74cd79d1
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2023-05-14 12:18:25 +0100

    Created my CustomerControllers and StaffController
    Added my CustomerController to enable basic CRUD operation to the customer model in the database.

---
+++ src/backend/web_ui/TWMS.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TWMS.Application.Contracts;
+using TWMS.Domain.Models.Customer;
+using System.Text.Json;
+using TWMS.Infrastructure_Help.Post_Model.Customer;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TWMS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        public CustomersController(ICustomerService customerService )
+        {
+            _customerService = customerService;
+        }
+
+        // Get all customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customers>>> GetAllCustomer()
+        {
+            var result = await _customerService.GetAllAsync(false);
+            return Ok(result);
+        }
+
+        //Get by Id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customers>> GetCustomerById(int id)
+        {
+            var customer = await _customerService.GetByIdAsync(false, id);
+            if(customer is null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        //Add customer to DB
+        [HttpPost]
+        public ActionResult AddCustomer([FromBody] CustomerRecord record )
+        {
+            Customers customer = new()
+            {
+                FullName = record.fullName,
+                Birthday = record.birthday,
+                PhoneNo = record.phoneNo,
+                EmailAddress = record.emailAddress,
+                CustomerAddress =
+                 new CustomerAddress()
+                 {
+                     BuildingNumber = record.buildingNumber,
+                     StreetName = record.streetName,
+                     AreaLocality = record.areaLocality,
+                     Landmark = record.landmark,
+                     City = record.city,
+                     Zip_PostalCode = record.zipCode,
+                     State_Province = record.stateProvince,
+
+                 }
+            };
+            _customerService.Add(customer);
+            return CreatedAtAction(nameof(GetCustomerById), new {id=customer.Id}, customer);//Ask ChatGPT for better explanation
+        }
+        //Deletes customer from DB
+        [HttpDelete("{id}")]
+        public void RemoveCustomer(int id)
+        {
+            _customerService.DeleteById(id);
+        }
+
+        // Update customer on DB
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Customers customer )
+        {
+            if(id!=customer.Id)
+            {
+                return BadRequest();
+            }
+            _customerService.Update(customer);
+            return NoContent();
+        }
+        [HttpGet("count")]
+        public Task<int> GetCustomerCount() 
+        {
+            var count = _customerService.GetTotalNumberOfCustomersAsync();
+            return count;
+        }
+        /*[HttpGet("search")]
+        public ActionResult Get(string searchTerm) 
+        {
+            return
+        }*/
+
+        
+    }
+}
---
+++ src/backend/web_ui/TWMS.WebAPI/Controllers/StaffController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TWMS.Application.Contracts;
+using TWMS.Domain.Models.Customer;
+using TWMS.Domain.Models.Staff;
+using TWMS.Infrastructure_Help.Post_Model.Staff;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TWMS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+      
+        public StaffController (IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+        // GET: api/<StaffController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Staff>>> Get()
+        {
+            var customers = await _staffService.GetAllAsync(false);
+            return Ok(customers);
+        }
+
+        // GET api/<StaffController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Staff>> Get(int id)
+        {
+            var customer =await _staffService.GetByIdAsync(false, id);
+            return Ok(customer);
+        }
+
+        // POST api/<StaffController>
+        [HttpPost]
+        public ActionResult Post([FromBody]StaffRecord record)
+        {
+            Staff staff = new()
+            {
+                FirstName = record.firstName,
+                LastName = record.lastName,
+                Birthday = record.birthday,
+                Gender = record.gender,
+                PhoneNo = record.phoneNo,
+                Salary = record.salary,
+                DateJoined = record.datejoined,
+                EmailAddress = record.emailAddress,
+                StaffAddress =
+                 new StaffAddress()
+                 {
+                     BuildingNumber = record.buildingNumber,
+                     StreetName = record.streetName,
+                     AreaLocality = record.areaLocality,
+                     Landmark = record.landmark,
+                     City = record.city,
+                     Zip_PostalCode = record.zipCode,
+                     State_Province = record.stateProvince,
+
+                 },
+                Guarantor = new Guarantor()
+                {
+                    FirstName = record.guarantorFirstName,
+                    LastName= record.guarantorLastName,
+                    Birthday= record.guarantorBirthday,
+                    Gender= record.guarantorGender,
+                    PhoneNo= record.guarantorPhoneNo,
+                    EmailAddress=record.guarantorEmail
+                }
+            };
+            _staffService.Add(staff);
+            return CreatedAtAction(nameof(Staff), new { id = staff.Id }, staff);//Ask ChatGPT for better explanation
+        }
+
+        // PUT api/<StaffController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Staff staff)
+        {
+            if(id!=staff.Id)
+            {
+                return BadRequest();
+            }
+            _staffService.Update(staff);
+            return NoContent();
+        }
+
+        // DELETE api/<StaffController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _staffService.DeleteById(id); 
+            return NoContent();
+        }
+    }
+}
